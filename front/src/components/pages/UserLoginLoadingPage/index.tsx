fc729c61ef147539025381de3e0ebc06f63393d6
JunHyuk Park <inte99ral@gmail.com>
2022-11-24 13:14:38 +0900

    🐛 fix(login): login-hoxfix

--- front/src/components/pages/UserLoginLoadingPage/index.tsx
+++ front/src/components/pages/UserLoginLoadingPage/index.tsx
@@ -1,4 +1,5 @@
 // API & Library
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { auth } from 'api/rest';
@@ -14,24 +15,9 @@ const UserLoginLoadingPage = () => {
   const locate = useLocation();
 
   // 로그인 Lock
-  const clickHandler = async () => {
-    // IFFE
-    await (async () => {
-      const params = new URLSearchParams(locate.search.substring(1));
-      console.log('[code]:', params.get('code'));
-      console.log('[엑세드 토큰 발급 시도]');
-      try {
-        await auth.loginCallback('google', params.get('code') || '');
-      } finally {
-        location.href = localStorage.getItem('URL') || '/';
-      }
-    })();
-  };
-
-  // 로그인 UnLock
-  // useEffect(() => {
+  // const clickHandler = async () => {
   //   // IFFE
-  //   (async () => {
+  //   await (async () => {
   //     const params = new URLSearchParams(locate.search.substring(1));
   //     console.log('[code]:', params.get('code'));
   //     console.log('[엑세드 토큰 발급 시도]');
@@ -41,11 +27,27 @@ const UserLoginLoadingPage = () => {
   //       location.href = localStorage.getItem('URL') || '/';
   //     }
   //   })();
-  // }, []);
+  // };
+
+  // 로그인 UnLock
+  useEffect(() => {
+    // IFFE
+    (async () => {
+      const params = new URLSearchParams(locate.search.substring(1));
+      console.log('[code]:', params.get('code'));
+      console.log('[엑세드 토큰 발급 시도]');
+      try {
+        await auth.loginCallback('google', params.get('code') || '');
+      } finally {
+        location.href = localStorage.getItem('URL') || '/';
+      }
+    })();
+  }, []);
 
   return (
     <>
-      <div onClick={clickHandler}>로그인 중 입니다...</div>
+      {/* <div onClick={clickHandler}>로그인 중 입니다...</div> */}
+      <div>로그인 중 입니다...</div>
     </>
   );
 };
